c60ed9b75b613965f201e7ef0aa042700ac5600c
Apoorva Kothari <lit2021029@iiitl.ac.in>
2023-08-27 02:54:04 +0530

    �project init

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+interface HomePageProps {
+  path?: string
+}
+
+const HomePage: React.FC<HomePageProps> = () => {
+  return <div>CodeFlow</div>
+}
+
+export default HomePage
